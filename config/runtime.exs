161e88d6f586814b8c20348c8c921a50612465b7
Cameron Koehler-Schnur <42192713+CamSchnur@users.noreply.github.com>
2026-03-02 11:33:55 -0800

    add custom url support

--- config/runtime.exs
+++ config/runtime.exs
@@ -53,6 +53,7 @@ if config_env() == :prod do
 
   config :jeopardy, JeopardyWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: [ "https://welcometohell.net", "//welcometohell.net"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
